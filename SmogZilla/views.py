5f808ffd169408a87d2aa1b1c0b4deb3dccd217d
Darzu Catalin <cs.darzu@gmail.com>
2024-06-13 09:15:37 +0300

    finals

--- SmogZilla/views.py
+++ SmogZilla/views.py
@@ -1,7 +1,7 @@
 import http
 
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
+from django.shortcuts import render, redirect
 from .models import Contact
 # Create your views here.
 
@@ -16,19 +16,28 @@ chat_id = '5614463571'
 def index(request):
     return render(request, 'index.html')
 
+# def addReview(request):
+#     name = request.POST['name']
+#     email = request.POST['email']
+#     comment = request.POST['comment']
+#     review = Contact(name=name, email=email, comment=comment)
+#     review.save()
+#     request.close()
+#     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 def addReview(request):
-    name = request.POST['name']
-    email = request.POST['email']
-    comment = request.POST['comment']
-    review = Contact(name=name, email=email, comment=comment)
-    review.save()
-    request.close()
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        comment = request.POST.get('comment')
 
-    #bot = Bot(token=TELEGRAM_BOT_TOKEN)
-    #chat_id = '5614463571'
-    #message_data = 'test'
-    #bot.send_message(chat_id=chat_id, text=message_data)
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        if name and email and comment:
+            review = Contact(name=name, email=email, comment=comment)
+            review.save()
+            return redirect(request.META.get('HTTP_REFERER', '/'))
+        else:
+            return HttpResponseBadRequest("Missing fields in the form")
+    else:
+        return HttpResponseBadRequest("Invalid request method")
 
 
 
